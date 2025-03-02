89244c945439f530f3492191736712e698ea1ad8
b.bahadornia <b.bahadornia@charisma.ir>
2025-03-02 09:52:27 +0330

    Added beahviors.

---
+++ src/01-Framework/Extensions/MediatRExtension.cs
@@ -0,0 +1,21 @@
+﻿using App.Framework.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace App.Framework.Extensions;
+
+public static class MediatRExtension
+{
+    public static IServiceCollection AddMediatRServices(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddMediatR(cfg=>
+        {
+            cfg.RegisterServicesFromAssemblies(assemblies);
+            cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+        services.AddValidatorsFromAssemblies(assemblies);
+        return services;
+    }
+}
