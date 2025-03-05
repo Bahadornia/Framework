7b0552d270c62f9552e7517b51c9513f94d77b0a
b.bahadornia <b.bahadornia@charisma.ir>
2025-03-05 13:36:53 +0330

    Refactored redis settings.

---
+++ src/01-Framework/Extensions/CacheExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
+
+namespace App.Framework.Extensions;
+
+public static class CacheExtensions
+{
+    public static IServiceCollection AddRedis(this IServiceCollection services, Action<RedisSettings> config)
+    {
+        var newConfig = new RedisSettings();
+        config.Invoke(newConfig);
+        services.TryAddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            return ConnectionMultiplexer.Connect(newConfig.BaseUrl);
+        });
+        return services;
+    }
+}
---
+++ src/01-Framework/Extensions/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Framework.Extensions
+{
+    public class RedisSettings
+    {
+        public string BaseUrl { get; set; } = default!;
+        public string InstanceName { get; set; } = default!;
+    }
+}
